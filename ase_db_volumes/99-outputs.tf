bf87e5a665efb7c1d54ff2e076036ec9508342a5
Craig Watson <craig@bashton.com>
2016-06-17 14:58:55 +0100

    Typo

--- ase_db_volumes/99-outputs.tf
+++ ase_db_volumes/99-outputs.tf
@@ -1,3 +1,3 @@
 output "ebs_list" {
-  value = "${aws_ebs_volume.db_backup.id}:xvdc,${aws_ebs_volume.sybase.id}:xvdm,${aws_ebs_volume.sapdata1.id}:xvdn,${aws_ebs_volume.sapdata2.id}:xvdo,${aws_ebs_volume.sapdata3.id}:xvdp,${aws_ebs_volume.sapdata4.id}:xvdq,${aws_ebs_volume.saplog.id}:xvdr"
+  value = "${aws_ebs_volume.db_backups.id}:xvdc,${aws_ebs_volume.sybase.id}:xvdm,${aws_ebs_volume.sapdata1.id}:xvdn,${aws_ebs_volume.sapdata2.id}:xvdo,${aws_ebs_volume.sapdata3.id}:xvdp,${aws_ebs_volume.sapdata4.id}:xvdq,${aws_ebs_volume.saplog.id}:xvdr"
 }
